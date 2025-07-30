71eb328c04d349e427e1e16354d89c133f117574
keshavAgarwal11 <73520882+keshavAgarwal11@users.noreply.github.com>
2025-07-30 11:01:09 +0530

    Time: 0 ms (100%), Space: 17.6 MB (52.49%) - LeetHub

--- 0034-find-first-and-last-position-of-element-in-sorted-array/0034-find-first-and-last-position-of-element-in-sorted-array.cpp
+++ 0034-find-first-and-last-position-of-element-in-sorted-array/0034-find-first-and-last-position-of-element-in-sorted-array.cpp
@@ -4,17 +4,9 @@ public:
             
    int first = firstval(nums,target);
    int last = lastval(nums,target);
-
         return {first,last};
-
-
     }
-
-
-
-
-
-                           int firstval(vector<int>& nums , int target){
+             int firstval(vector<int>& nums , int target){
                  int n = nums.size();
              int first = -1;
               int low =0;
@@ -23,49 +15,20 @@ public:
           while(low <= high){
 
          int mid = (low+high)/2;
-         
-                
                if(nums[mid] >= target){
-
-
                     if(nums[mid] == target){
                         first = mid;
                     }
                        high = mid-1;  
                }
-
-
                else{
 
                        low = mid+1;  
-
-
-
                }
 
-
-
-
-
           }
-
-
-
               return first;
-
-
          }
-
-
-
-
-
-
-
-
-
-
-         
                   int lastval(vector<int>& nums , int target){
                 int n = nums.size();
 int last =-1;
