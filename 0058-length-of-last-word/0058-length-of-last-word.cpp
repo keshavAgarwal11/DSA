7622243ff93f3498e9e53d6078723f395a98d221
keshavAgarwal11 <73520882+keshavAgarwal11@users.noreply.github.com>
2025-08-02 11:15:38 +0530

    Time: 0 ms (100%), Space: 8.9 MB (31.78%) - LeetHub

--- 0058-length-of-last-word/0058-length-of-last-word.cpp
+++ 0058-length-of-last-word/0058-length-of-last-word.cpp
@@ -1,44 +1,22 @@
 class Solution {
 public:
     int lengthOfLastWord(string s) {
-        
                int count = 0;
        int n = s.size(); 
-
-
        int i = n-1;
        while(s[i] == ' '){
-
         i--;
-       }
-                     
-       for(i; i>=0 ;i--){
-        
-                                             
+       }             
+       for(i; i>=0 ;i--){                                     
            if(s[i] != ' '){
-
             count++;
            }
            else {
             break;
            }
 
-
-
-
-
        }
-
-
            return count;
-
-
-
-
-
-
-
-
 
 
 
