bbcbecc39e7dd77adeda89ced5ee4fb5e01b42b4
keshavAgarwal11 <73520882+keshavAgarwal11@users.noreply.github.com>
2025-08-03 11:11:47 +0530

    Time: 0 ms (100%), Space: 8.3 MB (82.15%) - LeetHub

--- 1341-split-a-string-in-balanced-strings/1341-split-a-string-in-balanced-strings.cpp
+++ 1341-split-a-string-in-balanced-strings/1341-split-a-string-in-balanced-strings.cpp
@@ -1,24 +1,15 @@
 class Solution {
 public:
     int balancedStringSplit(string s) {
-         
-
   int n = s.size();
-
      int count3 = 0;
                int count1 = 0;
                int count2 = 0;
-
      for(char ch : s){
-           
-
              if(ch == 'R') count1++;
             else{
-
                 if(ch == 'L') count2++;
             }
-
-
             if(count1 == count2){
 
                 count3++;
@@ -26,27 +17,10 @@ public:
                 count2 = 0;
             }
 
-
-
-
-
-
-
-
-
      }
    
- 
-
 
               return count3;
-
-
-
-
-
-
-
 
     }
 };
