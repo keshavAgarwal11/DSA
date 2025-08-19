44ff0f37ab4c68d9db00a6d5b39733be133f66e7
keshavAgarwal11 <73520882+keshavAgarwal11@users.noreply.github.com>
2025-08-19 12:10:59 +0530

    Time: 0 ms (100%), Space: 8.4 MB (92.09%) - LeetHub

--- 0050-powx-n/0050-powx-n.cpp
+++ 0050-powx-n/0050-powx-n.cpp
@@ -1,12 +1,8 @@
 class Solution {
 public:
     double myPow(double x, int n) {
-        
-  
+    
        if(n==0) return 1;
-
-
-
         long long exp = n;
        if(exp<0){
              x = 1/x;
@@ -26,10 +22,5 @@ public:
      }
  
     } 
-
-
-
-
-
 
 };
