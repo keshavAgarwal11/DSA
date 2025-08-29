54aef142d2f332e79fcf73e6ba095067e58a7de2
keshavAgarwal11 <73520882+keshavAgarwal11@users.noreply.github.com>
2025-08-29 16:48:10 +0530

    Time: 7 ms (11.52%), Space: 12.1 MB (96.55%) - LeetHub

--- 0496-next-greater-element-i/0496-next-greater-element-i.cpp
+++ 0496-next-greater-element-i/0496-next-greater-element-i.cpp
@@ -1,72 +1,29 @@
 class Solution {
 public:
     vector<int> nextGreaterElement(vector<int>& nums1, vector<int>& nums2) {
-        
-                      
-    //    vector<int> ans;
-    // int n1 = nums1.size();
-    // int n2 = nums2.size();
-    //      bool val = 0;
-    //  for(int i=0;i<n1;i++){
-
-    //     for(int j=0;j<n2;j++){
-    //       if(nums2[j]== nums1[i]){
-
-          
-    //       if(nums2[j] > nums1[i]){
-    //             val =1;
-    //         ans.push_back(nums2[j]);
-    //         break; 
-
-    //       }
-
-    //       }
-
-    //     //   else{
-    //     //     ans.push_back(-1);
-    //     //   }
-
-         
-    //     }
-
-    //     if(!val) ans.push_back(-1);
-
-             
-
-
-    //  }  
-     
-
-    //     return ans;     
-
-
-
-
-
-
-
-
-
- vector<int> ans;
-        
-        for (int i = 0; i < nums1.size(); i++) {
-            int nextGreater = -1;
-            // find nums1[i] in nums2
-            for (int j = 0; j < nums2.size(); j++) {
-                if (nums2[j] == nums1[i]) {
-                    // search to the right for next greater
-                    for (int k = j + 1; k < nums2.size(); k++) {
-                        if (nums2[k] > nums1[i]) {
-                            nextGreater = nums2[k];
-                            break;
-                        }
-                    }
-                    break; // no need to search further once nums1[i] is found
-                }
-            }
-            ans.push_back(nextGreater);
-        }
+    vector<int> ans;
+  int n1 = nums1.size();
+     int n2 = nums2.size();
+      bool val = 0;
+    for(int i=0;i<n1;i++){
+          bool val = 0;
+        int nextgreater = -1;
+        for(int j =0; j<n2;j++){
+            if(nums1[i] == nums2[j]){
+                   val = 1;
+             for(int k = j+1; k<n2;k++){
+              if(nums2[k] > nums1[i]){
+                  nextgreater = nums2[k];
+                  break;
+ }
+             }   
+            } 
+             if(val){
+              ans.push_back(nextgreater);
+              break;
+             }
+        } 
+    }
         return ans;
-
     }
 };
