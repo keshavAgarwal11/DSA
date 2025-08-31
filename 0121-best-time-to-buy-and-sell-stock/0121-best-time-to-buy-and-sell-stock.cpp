e2f0924ea5bf7bb87c2611641ff3927796e53fec
keshavAgarwal11 <73520882+keshavAgarwal11@users.noreply.github.com>
2025-08-31 13:05:26 +0530

    Time: 0 ms (100%), Space: 97.5 MB (7.14%) - LeetHub

--- 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.cpp
+++ 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.cpp
@@ -4,23 +4,7 @@ public:
                    
    int n = prices.size();
    int mini = prices[0];
-        int profit =0;   
-//    for(int i=0; i<n-1;i++){
-
-//     for(int j=i+1; j<n; j++){
-             
-//              int diff = 0;
-//           if(prices[j] >prices[i]){
-
-//                 diff = prices[j] - prices[i];
-
-//                  maxi = max(maxi,diff);
-
-//           }
-
-
-//     }   
-//    }        
+        int profit =0;           
          for(int i=1;i<n;i++){
 
             int diff = prices[i]-mini;
@@ -30,15 +14,7 @@ public:
                    mini = min(mini,prices[i]);
          }
            
-
-
      return profit;
-
-
-
-
-
-
 
 
 
