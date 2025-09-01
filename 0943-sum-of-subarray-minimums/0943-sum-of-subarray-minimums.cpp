96b59d9dc1ae24d379b1d6a900e759620676fe77
keshavAgarwal11 <73520882+keshavAgarwal11@users.noreply.github.com>
2025-09-02 01:57:15 +0530

    Time: 21 ms (28.54%), Space: 47.1 MB (63.2%) - LeetHub

--- 0943-sum-of-subarray-minimums/0943-sum-of-subarray-minimums.cpp
+++ 0943-sum-of-subarray-minimums/0943-sum-of-subarray-minimums.cpp
@@ -1,94 +1,124 @@
 class Solution {
 public:
+
+
+
+        
+      vector<int> findnse(vector<int>& arr){
+
+                    int n = arr.size();
+         vector<int>nse(n);
+         stack<int> st;
+          
+        for(int i=n-1; i>=0 ;i--){
+
+
+          while(!st.empty() && arr[st.top()] >= arr[i]){
+
+           
+                st.pop();
+
+          }
+
+          nse[i] = st.empty()? n : st.top();
+                  
+
+             st.push(i);
+
+
+        } 
+
+
+
+          return nse;
+
+
+
+
+      }
+
+
+
+
+
+
+
+
+
+
+
+              
+      vector<int> findpse(vector<int>& arr){
+
+                    int n = arr.size();
+         vector<int>pse(n);
+         stack<int> st; 
+          
+        for(int i=0; i<n ;i++){
+
+
+          while(!st.empty() && arr[st.top()] > arr[i]){
+
+           
+                st.pop();
+
+          } 
+
+          pse[i] = st.empty()? -1 : st.top();
+                  
+
+             st.push(i);
+
+
+        } 
+
+
+
+          return pse;
+
+
+
+
+      }
+
+
+
+
+
+
     int sumSubarrayMins(vector<int>& arr) {
-//              int mini = INT_MAX;
-            //  int sum = 0;
-            // int n = arr.size();
-//             stack<int> st;
-//    for(int i=0;i<n;i++)
-//    {
 
-//       for(int j=i;j<n;j++){
-
-//        st.push(arr[j]);
-       
-//        while(!st.empty()){
-
-//            mini = min(mini,st.top());
-
-//              sum = sum + mini;
-
-//        }
-
-//       }
-
-
-
-//       while(!st.empty()){
-
-//          st.pop();
-//       }
-
-
-       
-//           mini = 0;
-
-
-//    }
-
-//    return sum;
-
-
-
-// for(int i=0; i<n; i++) {
-//     int mini = arr[i];
-//     for(int j=i; j<n; j++) {
-//         mini = min(mini, arr[j]);  // directly track min
-//          sum += mini;
-//     }
-// }
-
-//    return sum;
-
-
-
-
-           const int MOD = 1e9 + 7;
+            int MOD = 1e9 + 7;
         int n = arr.size();
 
-        vector<int> left(n), right(n);
-        stack<pair<int,int>> st1, st2;
+                 long long total = 0;
+         vector<int>nse = findnse(arr);
+         vector<int>pse = findpse(arr);
+        
 
-        // Previous Smaller Element (strictly greater for left)
-        for(int i=0; i<n; i++) {
-            int count = 1;
-            while(!st1.empty() && st1.top().first > arr[i]) {
-                count += st1.top().second;
-                st1.pop();
-            }
-            left[i] = count;
-            st1.push({arr[i], count});
-        }
+          for(int i=0;i<n;i++){
 
-        // Next Smaller Element (greater OR equal for right)
-        for(int i=n-1; i>=0; i--) {
-            int count = 1;
-            while(!st2.empty() && st2.top().first >= arr[i]) {
-                count += st2.top().second;
-                st2.pop();
-            }
-            right[i] = count;
-            st2.push({arr[i], count});
-        }
+             int left = i - pse[i];
+             int right = nse[i] - i;
 
-        // Final Contribution
-        long long res = 0;
-        for(int i=0; i<n; i++) {
-            long long contrib = (1LL * arr[i] * left[i] * right[i]) % MOD;
-            res = (res + contrib) % MOD;
-        }
 
-        return (int)res;
+        // total+= (right*left*1LL*arr[i])%MOD;
+            total = (total + (1LL * arr[i] * left % MOD * right % MOD) % MOD) % MOD;
+
+
+          }
+
+           return (int)total;
+
+          
+
+
+
+
+
+
+
+
     
     }
 };
