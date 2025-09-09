627771d5d2924d600b3435fe62f327a0edd8e287
keshavAgarwal11 <73520882+keshavAgarwal11@users.noreply.github.com>
2025-09-09 23:56:25 +0530

    Time: 12 ms (30.92%), Space: 39.3 MB (5.23%) - LeetHub

---
+++ 0907-koko-eating-bananas/0907-koko-eating-bananas.cpp
@@ -0,0 +1,99 @@
+class Solution {
+public:
+
+
+
+ int maxi(vector<int>& arr){
+     
+      int n = arr.size();
+       int maxi = arr[0];
+       
+      
+      for(int i=1;i<n;i++){
+          
+            maxi = max(maxi,arr[i]);
+            
+            
+      }
+     
+       return maxi;
+     
+     
+ }
+
+
+
+
+   long long fun1(vector<int> arr,int nob){
+        
+        long long totaltime = 0;
+       
+      int n = arr.size();
+      
+      for(int i=0; i<n; i++){
+          
+        //   totaltime += ceil(double(arr[i])/double(nob));
+           
+         totaltime += (arr[i] + nob - 1) / nob;
+
+      }
+      
+      return totaltime;
+       
+       
+      
+       
+   }
+ 
+
+
+
+
+
+
+
+    int minEatingSpeed(vector<int>& arr, int k) {
+           
+    int ans = INT_MAX;        
+  int low = 1;
+  int high = maxi(arr);
+  while(low <= high){
+      
+     
+      
+       int mid = (low +high)/2;
+       
+       long long minhrs = fun1(arr,mid);
+       
+       
+       
+       if(minhrs <= k){
+           
+        ans = mid;
+        
+         high = mid-1;  
+          
+           
+       }
+       
+       
+       else{
+           
+            low = mid+1;
+           
+       }
+       
+        
+       
+
+      
+  }
+   
+   
+   
+   
+    return ans;
+   
+   
+    }
+};
